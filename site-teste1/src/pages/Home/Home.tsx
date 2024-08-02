2020da0d9d000ebe20c833677a142f53280f7d8f
brunatavares22 <btavares346@gmail.com>
2024-08-01 22:09:27 -0300

    feat: integrando mais elementos

--- site-teste1/src/pages/Home/Home.tsx
+++ site-teste1/src/pages/Home/Home.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import  Header  from "../../components/Header/Header";
 import  Header2  from "../../components/Header/Header2";
+import Banner1 from "../../assets/banner1.jpeg";
+import Imperfeitos from "../../assets/imperfeitos.webp"
+import "./Home.css"
 
 
 const Home = () => {
   return (
-    <>
+    <>cla
    <Header/>
    <Header2/>
+   <div className='banner'>
+    <img src={Banner1} alt="imagem banner" className='banner1'/>
+   </div>
+    <div className='p-a'>
+    <p className='p6'>MAIS VENDIDOS</p>
+    <a href="https://www.youtube.com/watch?v=rYKOuKaWEjg&list=RD4FaHeREoRUc&index=20">Ver mais</a>
+    </div>
+    <div className='card-1'>
+    <img src={Imperfeitos} alt="livro imperfeitos" className='imperfeitos' />
+    <p className='p7'>Imperfeitos</p>
+    </div>
    </>
   )
 }
